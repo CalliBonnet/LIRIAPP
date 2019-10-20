ff71f01bd6bab2710aaab46a7c62a620c774aa6a
Calli Bonnet <calli@paradox.ai>
2019-10-20 16:19:37 -0700

    added to js file

--- liri.js
+++ liri.js
@@ -1,6 +1,50 @@
 //this hides my spotify keys 
-const env = require("dotenv").config();
+// const env = require("dotenv").config();
+
 //this holds my spotify keys
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
+
 //this created a varbile for my Spofity Keys 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+console.log('LIRI Bot is loading');
+
+
+//Bands in Town API Key and End Point 
+
+let BandsInTown = {
+    apiKey: 'codingbootcamp',
+    endpoint: 'https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp'
+  };
+
+
+  function searchedBand (bandName, venueName, venueLocation, eventDate) {
+    
+    this.bandName = bandName;  
+    this.venueName = venueName; 
+    this.venueLocation = venueLocation; 
+    this.eventDate = eventDate;
+
+}
+
+const commandLineBands =  process.argv[2]; 
+
+
+bandDetials = function() {
+    if(commandLineBands === 'concert-this') {
+        console.log("this is working")
+      
+    } else {
+        console.log("this is NOT working")
+       
+    }
+}
+
+bandDetials(); 
+
+// console.log(bandDetials()); 
+
+// let bands = new searchedBands("band name", "vanue name", "venue location", "event date")
+
+// console.log(bands); 
+

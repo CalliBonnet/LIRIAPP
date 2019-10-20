1af72cb5db3a85dbb692981f4bcd9b27c4f10670
Calli Bonnet <calli@paradox.ai>
2019-10-20 16:30:32 -0700

    added to js - bands

--- liri.js
+++ liri.js
@@ -7,28 +7,32 @@
 //this created a varbile for my Spofity Keys 
 // var spotify = new Spotify(keys.spotify);
 
-console.log('LIRI Bot is loading');
-
 
 //Bands in Town API Key and End Point 
-
 let BandsInTown = {
     apiKey: 'codingbootcamp',
     endpoint: 'https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp'
   };
 
-
+//This is the template for your searched bands 
   function searchedBand (bandName, venueName, venueLocation, eventDate) {
+
+    console.log('============= Start of Band ============='); 
     
     this.bandName = bandName;  
     this.venueName = venueName; 
     this.venueLocation = venueLocation; 
     this.eventDate = eventDate;
-
 }
+//this is where you will dymanically create content based on the API 
+let band = new searchedBand ("band name", "vanue name", "venue location", "event date")
 
+//this is displaying the band details in the console 
+console.log(band); 
+
+
+//When the user types 'concert-this', it will call this funcation
 const commandLineBands =  process.argv[2]; 
-
 
 bandDetials = function() {
     if(commandLineBands === 'concert-this') {
@@ -42,9 +46,9 @@ bandDetials = function() {
 
 bandDetials(); 
 
-// console.log(bandDetials()); 
 
-// let bands = new searchedBands("band name", "vanue name", "venue location", "event date")
 
-// console.log(bands); 
+
+
+
 
